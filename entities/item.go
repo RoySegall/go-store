7265d096565a145b1fb42afbca84b8b337ee7765
Roy Segall <roy.segall@gizra.com>
2017-08-13 23:23:41 +0300

    Change the structure.

--- entities/item.go
+++ entities/item.go
@@ -1,7 +1,7 @@
 package entities
 
 type Item struct {
-	Id 		string 	`json,gorethink:"id,omitempty"`
+	Id 		string 	`json,gorethink:"id,Id"`
 	Title string 	`json,gorethink:"title"`
 	Price float64 `json,gorethink:"price"`
 	Image string 	`json,gorethink:"image"`
