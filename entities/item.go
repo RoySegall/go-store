b7784a0b801524d3057addc980432a2be4811a23
Roy Segall <roy.segall@gizra.com>
2017-08-17 20:15:07 +0300

    Start to work on the items list.

--- entities/item.go
+++ entities/item.go
@@ -1,8 +1,9 @@
 package entities
 
 type Item struct {
-	Id 		string 	`json,gorethink:"id"`
-	Title string 	`json,gorethink:"title"`
-	Price float64 `json,gorethink:"price"`
-	Image string 	`json,gorethink:"image"`
+	Id 					string 	`json,gorethink:"id"`
+	Title 			string 	`json,gorethink:"title"`
+	Description string 	`json,gorethink:"description"`
+	Price 			float64 `json,gorethink:"price"`
+	Image 			string 	`json,gorethink:"image"`
 }
